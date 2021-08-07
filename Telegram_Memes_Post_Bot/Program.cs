1b07f2b6d76f741e8b0629d097415729bc5254cb
shurya <shuryaao@gmail.com>
2021-08-07 13:27:26 +0300

    Initial template asp.net core

---
+++ Telegram_Memes_Post_Bot/Program.cs
@@ -0,0 +1,82 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Net;
+
+namespace Telegram_Memes_Post_Bot
+{
+    public class Program
+    {
+        public static readonly string Namespace = typeof(Program).Namespace;
+        public static readonly string AppName = Namespace.Substring(Namespace.LastIndexOf('.', Namespace.LastIndexOf('.') - 1) + 1);
+        public static int Main(string[] args)
+        {
+            var configuration = GetConfiguration();
+
+            try
+            {
+                var host = BuildHost(configuration, args);
+
+                host.Run();
+
+                return 0;
+            }
+            catch (Exception)
+            {
+                return 1;
+            }
+        }
+
+        private static IHost BuildHost(IConfiguration configuration, string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webHost =>
+                {
+                    webHost.CaptureStartupErrors(false)
+                    .ConfigureKestrel(options =>
+                    {
+                        var (httpPort, healthCheckPort) = GetDefinedPorts(configuration);
+
+                        options.Listen(IPAddress.Any, httpPort, listenOptions =>
+                        {
+                            listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+                        });
+
+                        if (httpPort != healthCheckPort)
+                        {
+                            options.Listen(IPAddress.Any, healthCheckPort, listenOptions =>
+                            {
+                                listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+                            });
+                        }
+                    })
+                    .UseStartup<Startup>()
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .UseConfiguration(configuration);
+                })
+                .Build();
+
+        private static IConfiguration GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            return builder.Build();
+        }
+
+
+        private static (int httpPort, int healthCheckPort) GetDefinedPorts(IConfiguration config)
+        {
+            var httpPort = config.GetValue("PORT", 5014);
+            var healthCheckPort = config.GetValue("HEALTH_CHECK_PORT", 5015);
+            return (httpPort, healthCheckPort);
+        }
+
+    }
+}
---
+++ Telegram_Memes_Post_Bot/Startup.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Telegram_Memes_Post_Bot
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHealthChecks();
+
+            services.AddControllers().AddJsonOptions(json =>
+            {
+                json.JsonSerializerOptions.PropertyNamingPolicy = null;
+            });
+
+            services.AddOptions();
+           
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder
+                    .SetIsOriginAllowed((host) => true)
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+
+            services.AddResponseCompression();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            var pathBase = Configuration["PATH_BASE"];
+
+            if (!string.IsNullOrEmpty(pathBase))
+            {
+                app.UsePathBase(pathBase);
+            }
+
+            app.UseRouting();
+
+            app.UseResponseCompression();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseEndpoints(endpoints =>
+            {
+                var healthCheckPort = Configuration.GetValue("HEALTH_CHECK_PORT", 5015);
+
+                endpoints.MapHealthChecks("/readiness").RequireHost($"*:{healthCheckPort}");
+                endpoints.MapHealthChecks("/liveness").RequireHost($"*:{healthCheckPort}");
+
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
